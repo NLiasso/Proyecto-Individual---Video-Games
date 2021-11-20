09c517590dc4997483e6a018dfae4a6373343585
Nliasso <nliasso@hotmail.com>
2021-11-19 21:23:12 -0300

    Avances en el front

--- client/src/components/game.jsx
+++ client/src/components/game.jsx
@@ -4,8 +4,8 @@ export default function Game({name, image, genres, id, description}){
     return <div> 
         <Link to={`/${id}`}>
             <h2> {name} </h2>
+        </Link>
             <img src={image} style={{width: '20vw'}} alt= 'portada de juego'/>
             <h4> {genres} </h4>
-        </Link>
     </div>
 }
--- client/src/components/gameDetail.jsx
+++ client/src/components/gameDetail.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useParams } from "react-router"
+import parse from 'html-react-parser'
+import { Link } from "react-router-dom"
+
 
 export default function GameDetail(){
 
@@ -16,16 +19,34 @@ export default function GameDetail(){
         } // clean up
     }, [])
         return <div> 
+                <Link to='/home'>
+            <button>To Home</button>
+            </Link>
         {
+        
             game ?
             <>
             <h2> {game.name} </h2>
-            <h4> {game.description} </h4>
-            <h4> Id del Juego: {id} </h4>
-            <img src={game.image} style={{width: '20vw'}} alt= 'portada de juego' /> 
-            <h4 genres={game.genres} />
+            <img src={game.background_image} style={{width: '20vw'}} alt= 'portada de juego' /> 
+            <h4> Generos: {game.genres.map(g => g.name).join(", ")} </h4>
+            <h4> Descripción del Juego: <br />
+                 {parse(game.description)} </h4>
+            <h4> Fecha de lanzamiento: {game.released} </h4>
+            <h4> Rating: {game.rating} </h4>
+            <h4> Plataformas compatibles: {game.platforms.join(", ")} </h4>
             </> :
             <div> cargando </div>
         }
     </div>
 }
+
+
+/*
+[X] Los campos mostrados en la ruta principal para cada videojuegos (imagen, nombre, y géneros)
+[X] Descripción
+[X] Fecha de lanzamiento
+[X] Rating
+[X] Plataformas
+
+
+*/
